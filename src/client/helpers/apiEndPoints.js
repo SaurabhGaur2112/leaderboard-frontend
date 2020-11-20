2834523822a9ea675058542363136531b37039be
SaurabhGaur68 <gaursaurabh68@gmail.com>
2020-11-20 14:52:30 +0530

    add create player endpoints

--- src/client/helpers/apiEndPoints.js
+++ src/client/helpers/apiEndPoints.js
@@ -1,7 +1,7 @@
 const apiEndPoints = {
   leader: {
     getLeaders: () => '/api/v1/leaderboard/',
-    createLeader: () => '/api/v1/players/score-add/',
+    createLeader: () => '/api/v1/player/create/',
   },
 };
 
